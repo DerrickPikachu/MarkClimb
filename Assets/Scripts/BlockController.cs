fcbf08cca6d29af752191eecbbe744a18ebe7ab6
skps2010 <skps2010030908@yahoo.com>
2022-12-06 17:32:55 +0800

    add sounds nd particles

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -8,10 +8,17 @@ public class BlockController : MonoBehaviour
 
     public float speed;
     public GameObject player;
-    public GameObject particle;
+    public GameObject teleportParticle;
+    public GameObject squashParticle;
+    public GameObject placeParticle;
+    public AudioSource audioSource;
+    public AudioClip teleportSound;
+    public AudioClip placeSound;
+    public AudioClip squashSound;
     public Material[] materials = new Material[2];
     private bool isOnFloor = false;
     private bool isPortal = false;
+    private bool isCollidedWithPlayer = false;
 
     public void Init(bool isPortal)
     {
@@ -52,12 +59,21 @@ public class BlockController : MonoBehaviour
             isOnFloor = true;
             transform.position = rightPos;
             GameManager.instance.blockMap[x, y, z] = true;
+            Instantiate(placeParticle, rightPos, Quaternion.identity).SetActive(true);
+            audioSource.PlayOneShot(placeSound);
+
+            if (isCollidedWithPlayer && IsInSamePlace())
+            {
+                GameObject o = Instantiate(squashParticle, rightPos, Quaternion.identity);
+                o.SetActive(true);
+                audioSource.PlayOneShot(squashSound);
+            }
 
             if (isPortal)
             {
                 var pos = transform.position;
                 pos.y += GetComponent<BoxCollider>().size.y;
-                GameObject o = Instantiate(particle, pos, Quaternion.identity);
+                GameObject o = Instantiate(teleportParticle, pos, Quaternion.identity);
                 o.GetComponent<ParticleSystem>().loop = true;
                 o.SetActive(true);
             }
@@ -66,21 +82,33 @@ public class BlockController : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
-        if (!isPortal)
-            return;
-
         if (other.gameObject == player)
         {
-            Vector3 size = GetComponent<BoxCollider>().size;
-            Vector3 diff = player.transform.position - transform.position;
-            if (diff.y < 0 || diff.x > size.x || diff.x < -size.x || diff.z > size.z || diff.z < -size.z)
+            isCollidedWithPlayer = true;
+            if (!IsInSamePlace() || !isPortal)
                 return;
 
             Vector3 pos = player.transform.position;
             pos.y += 10;
             player.transform.position = pos;
-            GameObject o = Instantiate(particle, pos, Quaternion.identity);
+            GameObject o = Instantiate(teleportParticle, pos, Quaternion.identity);
             o.SetActive(true);
+            audioSource.PlayOneShot(teleportSound);
         }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject == player)
+        {
+            isCollidedWithPlayer = false;
+        }
+    }
+
+    private bool IsInSamePlace()
+    {
+        Vector3 size = GetComponent<BoxCollider>().size;
+        Vector3 diff = player.transform.position - transform.position;
+        return diff.x <= size.x && diff.x >= -size.x && diff.z <= size.z && diff.z >= -size.z;
     }
 }
