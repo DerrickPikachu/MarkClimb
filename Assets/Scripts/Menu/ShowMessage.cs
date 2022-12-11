7207dfe817767b3771890b9c0c371a33b239a17e
ksshen0000 <shenkexuannctu@gmail.com>
2022-12-11 16:36:43 +0800

    add moving text

---
+++ Assets/Scripts/Menu/ShowMessage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ShowMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string welcomeMessage;
+    private int curPos;
+    private float tTimer;
+    TMP_Text tt;
+    public float gap;
+    void Start()
+    {
+        tt = GetComponent<TMP_Text>();
+        Debug.Log(tt);
+        curPos = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tTimer = tTimer + Time.deltaTime;
+        if (tTimer >= gap)
+        {
+            curPos++;
+            tTimer = 0;
+        }
+        if (curPos < welcomeMessage.Length)
+        {
+            tt.text = welcomeMessage.Substring(0, curPos);
+        }
+    }
+}
