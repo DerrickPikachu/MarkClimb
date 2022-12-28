fbbc548c2d946bf11b3a7241ecfd7eab2dfd469d
DerrickPikachu <derrfgh4563@gmail.com>
2022-12-28 17:44:29 +0800

    make player big jump and little jump more easily

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public enum EffectType
 {
     SpeedUp,
     SpeedDown,
-    JumpUp
+    JumpUp,
+    DoubleJump
 }
 public class PlayerController : MonoBehaviour
 {
@@ -26,6 +27,8 @@ public class PlayerController : MonoBehaviour
     public float upGravityScale = 5;
     public float downGravityScale = 10;
     public float runSpeedUpFactor = 2.0f;
+    public float maxJumpButtonTime = 0.3f;
+    public GameObject doubleJumpEffect;
 
     private Vector3 forwardZ = new Vector3(0, 0, 1.0f);
     private Vector3 backwardZ = new Vector3(0, 0, -1.0f);
@@ -33,9 +36,14 @@ public class PlayerController : MonoBehaviour
     private Quaternion rotateTarget = Quaternion.identity;
     private Rigidbody rb;
     private BoxCollider boxCollider;
+    private float zeroThreshold = 0.01f;
     private float gravityScale;
     private Animator anim = null;
     private bool jumping = false;
+    private bool doubleJump = false;
+    private int jumpChance = 1;
+    private float jumpTime = 0;
+    private float jumpDownForce = 10f;
     private Dictionary<EffectType, float> effects = new Dictionary<EffectType, float>();
     private Vector3 allJumpForce;
     private Vector3 maxJumpForce = new Vector3(0, 30, 0);
@@ -62,6 +70,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         HandleKey();
+        UpdateJumpTime();
         UpdateGravityScale();
         ForceXAxis();
         UnSquash();
@@ -96,27 +105,40 @@ public class PlayerController : MonoBehaviour
 
     private void HandleKey()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded()) {
-            // rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jumping = true;
-            allJumpForce = Vector3.zero;
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (isGrounded()) {
+                jumpChance = (HasEffect(EffectType.DoubleJump) ? 2 : 1);
+                jumping = true;
+                jumpTime = 0;
+            // } else if (HasEffect(EffectType.DoubleJump)) {
+            } else if (jumpChance > 0) {
+                doubleJump = true;
+                jumpTime = 0;
+            }
         }
         if (Input.GetKeyUp(KeyCode.UpArrow)) {
             jumping = false;
+            doubleJump = false;
         }
     }
 
     private void Jump()
     {
-        if (jumping) {
-            var upForce = Vector3.up * jumpForce;
-            allJumpForce += upForce;
-            if(allJumpForce.y > maxJumpForce.y)
-            {
-                upForce.y -= allJumpForce.y - maxJumpForce.y;
-                jumping = false;
+        if (jumpChance > 0) {
+            if (jumping && isGrounded()) {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                jumpChance--;
             }
-            rb.AddForce(upForce, ForceMode.Impulse);
+            if (doubleJump) {
+                Instantiate(doubleJumpEffect, transform.position, Quaternion.identity);
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                doubleJump = false;
+                jumpChance--;
+            }
+        }
+        if (!jumping && jumpTime < maxJumpButtonTime) {
+            rb.AddForce(-Vector3.up * jumpDownForce, ForceMode.Impulse);
+            jumpTime = maxJumpButtonTime;
         }
     }
 
@@ -131,10 +153,7 @@ public class PlayerController : MonoBehaviour
         if(HasEffect(EffectType.SpeedDown))
             speed *= 0.2f;
         if(HasEffect(EffectType.JumpUp))
-        {
-            rb.AddForce(maxJumpForce * 1.414f, ForceMode.Impulse);
-            RemoveEffect(EffectType.JumpUp);
-        }
+            rb.AddForce(Vector3.up * jumpForce * 1f, ForceMode.Impulse);
 
         if (anim != null) {
             anim.SetFloat("Speed", Mathf.Abs(speed));
@@ -169,6 +188,7 @@ public class PlayerController : MonoBehaviour
     {
         return (input > 0 && currentDirection == Direction.Left) || (input < 0 && currentDirection == Direction.Right);
     }
+
     public void AddEffect(EffectType effectType, float sec)
     {
         float newDue = (float)Time.timeAsDouble + sec;
@@ -177,6 +197,7 @@ public class PlayerController : MonoBehaviour
             effects[effectType] = newDue;
         }
     }
+
     public bool HasEffect(EffectType effectType)
     {
         if(effects.ContainsKey(effectType) && effects[effectType] < Time.timeAsDouble)
@@ -221,6 +242,13 @@ public class PlayerController : MonoBehaviour
             transform.position = pos;
             SoundManager.instance.PlaySound(SoundClip.Portal);
             ParticleManager.instance.SpawnParticle(Particle.Teleport, pos);
+        }
+    }
+
+    private void UpdateJumpTime()
+    {
+        if (jumping) {
+            jumpTime += Time.deltaTime;
         }
     }
 }
