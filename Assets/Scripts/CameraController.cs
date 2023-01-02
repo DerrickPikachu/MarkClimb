5ec28249ce3ba414ec74de13634c02a26148eb85
DerrickPikachu <derrfgh4563@gmail.com>
2023-01-02 20:45:00 +0800

    add skill flash in player controller (need refactor)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    public float smooth = 0.3f;
+    public Vector3 cameraOffset = new Vector3(15, 3, 0);
 
-    private Vector3 cameraOffset = new Vector3(15, 3, 0);
+    private Vector3 moveVelocity = Vector3.zero;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = player.transform.position + cameraOffset;
+        transform.LookAt(player.transform);
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -22,7 +28,10 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.position = player.transform.position + cameraOffset;
-        transform.LookAt(player.transform);
+        Vector3 cameraPosTarget = player.transform.position + cameraOffset;
+        Vector3.SmoothDamp(transform.position, cameraPosTarget, ref moveVelocity, smooth);
+        rb.velocity = moveVelocity;
+        // transform.position = player.transform.position + cameraOffset;
+        // transform.LookAt(player.transform);
     }
 }
